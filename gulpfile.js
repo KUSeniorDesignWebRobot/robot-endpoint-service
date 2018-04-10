2312486d6229fe1a426c25c77754f46c7beea911
johnmccain <johnm.freestate@gmail.com>
2018-04-10 13:21:31 -0500

    Change gulp configuration settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,40 @@
-const gulp = require('gulp');
-const nodemon = require('gulp-nodemon');
-const plumber = require('gulp-plumber');
-const livereload = require('gulp-livereload');
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const nodemon = require("gulp-nodemon");
+const plumber = require("gulp-plumber");
+const livereload = require("gulp-livereload");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
 
-gulp.task('sass', () => {
-  gulp.src('./public/css/*.scss')
+gulp.task("sass", () => {
+  gulp
+    .src("./public/src/css/*.scss")
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest('./public/css'))
+    .pipe(gulp.dest("./public/css"))
     .pipe(livereload());
 });
 
-gulp.task('watch', () => {
-  gulp.watch('./public/css/*.scss', ['sass']);
+gulp.task("js", () => {
+  gulp
+    .src("./public/src/js/*.js")
+    .pipe(babel({ presets: ["env"] }))
+    .pipe(gulp.dest("./public/js"))
+    .pipe(livereload());
 });
 
-gulp.task('develop', () => {
+gulp.task("watch", () => {
+  gulp.watch("./public/src/css/*.scss", ["sass"]);
+  gulp.watch("./public/src/js/*.js", ["js"]);
+});
+
+gulp.task("develop", () => {
   livereload.listen();
   nodemon({
-    script: 'bin/www',
-    ext: 'js handlebars coffee',
+    script: "bin/www",
+    ext: "js handlebars coffee",
     stdout: false
-  }).on('readable', function () {
-    this.stdout.on('data', (chunk) => {
+  }).on("readable", function() {
+    this.stdout.on("data", chunk => {
       if (/^Express server listening on port/.test(chunk)) {
         livereload.changed(__dirname);
       }
@@ -33,8 +44,4 @@ gulp.task('develop', () => {
   });
 });
 
-gulp.task('default', [
-  'sass',
-  'develop',
-  'watch'
-]);
+gulp.task("default", ["sass", "js", "develop", "watch"]);
